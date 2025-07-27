b9cf5531bfb93fba421307c35c25a99d674255b0
Christine Nyambwari <christinenyambwari@gmail.com>
2025-07-27 16:44:14 -0700

    Remove dist folder from git tracking

--- dist/users/validator.js
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-export const usersSchema = z.object({
-    full_name: z.string(),
-    email: z.string(),
-    contact_phone: z.string(),
-    address: z.string(),
-    role: z.string()
-});
