9cd066d252be7089b218b3626f22b6392fa8cb23
Christine Nyambwari <christinenyambwari@gmail.com>
2025-08-09 08:42:20 -0700

    sales search query

--- src/routes/provider/services.ts
+++ src/routes/provider/services.ts
@@ -2,12 +2,26 @@
 import { Hono } from 'hono';
 import { db } from '../../drizzle/db.js';
 import { services } from '../../drizzle/schema.js';
-
+import { ilike,or} from 'drizzle-orm';
 const serviceRoutes = new Hono();
 
 serviceRoutes.get('/api/services', async (c) => {
   try {
-    const result = await db.select().from(services);
+    const search = c.req.query('search');
+    
+    const baseQuery = db.select().from(services);
+    
+    const query = search 
+      ? baseQuery.where(
+          or(
+            ilike(services.name, `%${search}%`),
+            ilike(services.category, `%${search}%`),
+            ilike(services.description, `%${search}%`)
+          )
+        )
+      : baseQuery;
+    
+    const result = await query;
     return c.json(result);
   } catch (err) {
     console.error('Error fetching services:', err);
